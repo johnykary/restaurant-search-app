26ae96efe0a07dd5f4d0936b9bebc22c7c2cb8c8
giannis.karystinos <giannis.karystinos@gugroup.com>
2021-03-14 00:27:44 +0200

    Api call

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,32 @@
 import React, {useState} from 'react';
-
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from './components/SearchBar';
+import yelp from './api/yelp';
 
 
 const SearchScreen = () => {
-
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, seterrorMessage] = useState('')
+
+    const searchApi = async (searchTerm) =>{
+        try{
+            const response = await yelp.get('/search',{
+                params:{
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses)
+        }catch(e){
+            seterrorMessage('Something went wrong')
+        }
+        
+    }
+
+    //Bad way of initializing first search
+    // searchApi('pasta');
 
 
     return(
@@ -14,10 +34,12 @@ const SearchScreen = () => {
             <SearchBar
                 term = {term}
                 onTermChange = {(newTerm => setTerm(newTerm))}
-                onTermSubmit = {() => console.log('term was sub')}
+                onTermSubmit = {() => searchApi(term)}
             />
             
-            <Text> Search Screen</Text>
+            {errorMessage ? <Text> {errorMessage}</Text> : null}
+
+            <Text>We have found {results.length} results</Text>
         </View>
     )
 }
