e10bd2f4698386cadbdca5d5c14ed68db50fd741
giannis.karystinos <giannis.karystinos@gugroup.com>
2021-03-17 21:57:20 +0200

    Hooks and ResultList

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,20 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from './components/SearchBar';
-import yelp from './api/yelp';
+import useResults from './hooks/useResults';
+import ResultsList from './components/ResultsList';
 
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, seterrorMessage] = useState('')
+    const [searchApi, results, errorMessage] = useResults();
 
-    const searchApi = async (searchTerm) =>{
-        try{
-            const response = await yelp.get('/search',{
-                params:{
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
-            setResults(response.data.businesses)
-        }catch(e){
-            seterrorMessage('Something went wrong')
-        }
-        
+    const filterResultsByPrice = (price) =>{
+          return results.filter(result =>{
+              return result.price === price
+          });  
     }
-
-    //Bad way of initializing first search
-    // searchApi('pasta');
-
-
+    
     return(
         <View>
             <SearchBar
@@ -40,6 +26,10 @@ const SearchScreen = () => {
             {errorMessage ? <Text> {errorMessage}</Text> : null}
 
             <Text>We have found {results.length} results</Text>
+
+            <ResultsList results ={filterResultsByPrice('$')} title='Cost Effective'/>
+            <ResultsList results ={filterResultsByPrice('$$')} title='Bit Pricier'/>
+            <ResultsList results ={filterResultsByPrice('$$$')} title='Bit Spender'/>
         </View>
     )
 }
---
+++ src/screens/hooks/useResults.js
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react';
+import yelp from '../api/yelp';
+
+
+/*
+    useEffect(() => {}) -> Run the arrow function every time the component is rendered
+    useEffect(() => {}, []) -> Run the arrow function only when  the component is first rendered
+    useEffect(() =>{}, [value]) => Run the arrow function only when  the component is first rendered and when the 'value' changes
+*/
+
+export default(() => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, seterrorMessage] = useState('')
+
+    const searchApi = async (searchTerm) =>{
+        try{
+            const response = await yelp.get('/search',{
+                params:{
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses)
+        }catch(e){
+            seterrorMessage('Something went wrong')
+        }
+        
+    }
+
+    useEffect(() =>{
+        searchApi('pasta')
+    }, []);
+
+    return [searchApi, results, errorMessage ]
+})
